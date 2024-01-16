e3d2e9cf655bbd1eddb6f38939674c2109709c65
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-01-16 13:06:09 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    Console.Write("Введите текст: ");
+    string text = Console.ReadLine();
+    if (text == "q")
+    {
+        break;
+    }
+    int number;
+    if(int.TryParse(text, out number)) // веденное значение состоит из цифр
+    {
+        Console.WriteLine("Введенная строка состоит из цифр");
+        int sum = 0;
+        while (number > 0)
+        {
+            sum = sum + number%10;
+            number/=10;
+        }
+        if (sum%2==0)
+        {
+            break;
+        }
+    }
+}
