ea444a6e8fc883b36bb04d513716489221e6cbd1
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-01-17 23:16:44 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = {11, 22, 33, 44, 55, 66};
+Console.Write("Массив: ");
+for (int i = 0; i < arr.Length; i++)
+{
+   Console.Write($"{arr [i]} "); 
+}
+int temp;
+for (int i = 0; i < arr.Length/2; i++)
+{
+    temp = arr[i];
+    arr[i] = arr[arr.Length - 1 - i];
+    arr[arr.Length - 1 - i] = temp;
+}
+Console.Write ("\nПеревернутый масив: ");
+for (int i = 0; i < arr.Length; i++)
+{
+   Console.Write($"{arr [i]} "); 
+}
